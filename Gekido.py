4b528ab20fd49466d70356ae71e9d68da83b3d82
Declan Kelly <dkelly2@mybce.catholic.edu.au>
2013-07-25 17:39:27 -0400

    Added answer, added c9 support, added Answer file, added more stuff to Gekido

--- Gekido.py
+++ Gekido.py
@@ -2,17 +2,19 @@
 # Import the maths
 import math
 import decimal
+import locale
 
+locale.setlocale(locale.LC_ALL, 'en_US')
 # Set it to be really accurate.
 decimal.getcontext().prec = 1000
 
-# Set my deltas to be really accurate
-k = decimal.Decimal("0.0000000001")
-delta = decimal.Decimal("0.0000000001")
+# Set my deltas to be really accurate22
+k = decimal.Decimal("0.00000000000000000000000000000000000000000001")
+delta = decimal.Decimal("0.00000000000000000000000000000000000000000001")
 
 # What are we making a start as
 a = decimal.Decimal("5.0")
-# Calculating the gradient with this
+# Calculating the gradient with thisvv
 m = abs(((a**k)-1)/k)
 
 # b is the value I am subtracting or adding for my binary search algorithm
@@ -23,18 +25,18 @@ while m != 1 + delta:
     # if the a value is too big, make it a bit smaller
     if m > 1:
         a = a - b
-        
+
     # if the a value is too small, make it a bit larger
     elif m < 1:
         a = a + b
-        
+
     # Make b smaller
     b = b / 2
-    
+
     # Recalculate the gradient
     m = abs(((a**k-1)/k))
     # Print the working total
-    print a
+    locale.format("%d", a, grouping=True)
 
 # Print it one last time when we are done!
 print a
